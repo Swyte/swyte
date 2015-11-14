07cfa422d60b7970b67b0a704bdeee71ca2f203c
Jacob Jenkins <jacob@jacobejenkins.com>
2015-11-14 10:07:07 -0500

    Refactor Instagram passport function

--- app/config/passport.js
+++ app/config/passport.js
@@ -5,7 +5,7 @@ let InstagramStrategy = require('passport-instagram').Strategy;
 let FacebookStrategy = require('passport-facebook').Strategy;
 let TwitterStrategy = require('passport-twitter').Strategy;
 let secrets = require('./config/secrets');
-let User = require('./models/user');
+let Users = require('./models/user');
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
@@ -21,58 +21,45 @@ passport.deserializeUser((id, done) => {
  * Sign in with Instagram.
  */
 passport.use(new InstagramStrategy(secrets.instagram, function(req, accessToken, refreshToken, profile, done) {
-    if (req.user) {
-        User.findOne({
-            instagram: profile.id
-        }, function(err, existingUser) {
-            if (existingUser) {
-                req.flash('errors', {
-                    msg: 'There is already an Instagram account that belongs to you. Sign in with that account or delete it, then link it with your current account.'
-                });
-                done(err);
-            } else {
-                User.findById(req.user.id, function(err, user) {
-                    user.instagram = profile.id;
-                    user.tokens.push({
-                        kind: 'instagram',
-                        accessToken: accessToken
-                    });
-                    user.profile.name = user.profile.name || profile.displayName;
-                    user.profile.picture = user.profile.picture || profile._json.data.profile_picture;
-                    user.profile.website = user.profile.website || profile._json.data.website;
-                    user.save(function(err) {
-                        req.flash('info', {
-                            msg: 'Instagram account has been linked.'
-                        });
-                        done(err, user);
-                    });
-                });
-            }
-        });
-    } else {
-        User.findOne({
-            instagram: profile.id
-        }, function(err, existingUser) {
-            if (existingUser) return done(null, existingUser);
+    Users.findOne({
+        phone: req.body.phone
+    }, (err, existingUser) => {
+        if (existingUser && existingUser.instagram) {
+            req.flash('errors', {
+                msg: 'There is already an Instagram account that belongs to you. Sign in with that account or delete it, then link it with your current account.'
+            });
+            // There's already an Instagram account associated with this user.
+            done(err);
+        } else if (existingUser) {
+            // User already exists but does not have their Instagram account
+            // attached.
+            existingUser.instagram = profile.id;
+            existingUser.tokens.push({
+                kind: 'instagram',
+                accessToken: accessToken
+            });
 
-            var user = new User();
+            existingUser.save(function(err) {
+                req.flash('info', {
+                    msg: 'Instagram account has been linked.'
+                });
+                done(err, existingUser);
+            });
+        } else {
+            var user = new Users();
+            user.phone = req.body.phone;
             user.instagram = profile.id;
             user.tokens.push({
                 kind: 'instagram',
                 accessToken: accessToken
             });
             user.profile.name = profile.displayName;
-            // Similar to Twitter API, assigns a temporary e-mail address
-            // to get on with the registration process. It can be changed later
-            // to a valid e-mail address in Profile Management.
-            user.email = profile.username + "@instagram.com";
-            user.profile.website = profile._json.data.website;
             user.profile.picture = profile._json.data.profile_picture;
             user.save(function(err) {
                 done(err, user);
             });
-        });
-    }
+        }
+    });
 }));
 
 /**
