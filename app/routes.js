9bed7d70dd8d12cab2c123300ffe52de1ca6e141
InternetExplorer7 <themaster231436@gmail.com>
2015-11-14 12:37:00 -0800

    Removed code that wasn't required.

--- app/routes.js
+++ app/routes.js
@@ -12,7 +12,7 @@ exports.text = (req, res) => {
 		if(!err && user){ // Found
 			if(user.facebook === '') { // Account not yet attached
 				console.log("No Facebook account found");
-				res.send("<Response><Message>Welcome back, we still need permission to access your Facebook account. https://fb.com/auth</Message></Response>");
+				res.send("<Response><Message>Welcome back, we still need permission to access your Facebook account. https://localhost:3000/auth/facebook</Message></Response>");
 			} else { // Account found and Facebook attached
 						   /* -----------------------------TEMPLATES ----------------------------- */
 				wit(req.body.Body, function (err, response) {
@@ -31,19 +31,7 @@ exports.text = (req, res) => {
 						  /* -----------------------------TEMPLATES ----------------------------- */
 			}
 		} else { // Not found, register new user
-			var adduser = new Users({
-				phone: req.body.From,
-				facebook: "auth"
-			});
-			adduser.save(function (err) {
-				if(err) {
-					console.log("Error " + err);
-					res.send("<Response><Message>Shit, we ran into an error. Give this to a dev: " + err + "</Message></Response>");
-				} else {
-					console.log("Saved succesfully!");
-					res.send("<Response><Message>Hey, to start things off we will need to get access to your Facebook account. https://fb.com/auth</Message></Response>");
-				}
-			});
+			res.send("<Response><Message></Message></Response>");
 		}
 	});
 };
