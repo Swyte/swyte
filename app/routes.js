0419ea8454cb11a1abeae8d2c7ad257ee65aa641
InternetExplorer7 <themaster231436@gmail.com>
2015-11-15 06:23:45 -0800

    a

--- app/routes.js
+++ app/routes.js
@@ -29,19 +29,21 @@ exports.text = function(req, res) {
     Users.findOne({
         phone: req.body.From
     }, (err, user) => {
-        console.log('poo2' + req.body.Body);
+    	console.log("USER: " + user);
         if (!err && user) { // Found
             if (user.facebook === '') { // Account not yet attached
                 console.log("No Facebook account found");
                 res.send(`<Response><Message>Welcome back, we still need permission to access your Facebook account. https://swyte.xyz/oauth/#!/facebook/${req.body.From.replace("+","")}</Message></Response>`);
             } else { // Account found and Facebook attached
+            	console.log("Hit ELSE");
                 /* -----------------------------TEMPLATES ----------------------------- */
                 wit(req.body.Body, function(err, response) {
                     if (!err && response) { // Handle templates, no error and response is valid
-                        console.log("Length: " + Object.keys(response.outcomes[0].entities).length);
-                        console.log("WIT RESPONSE: " + JSON.stringify(response));
+                        //console.log("Length: " + Object.keys(response.outcomes[0].entities).length);
+                        //console.log("WIT RESPONSE: " + JSON.stringify(response));
+                        console.log("USER: " + user);
                         if (Object.keys(response.outcomes[0].entities).length === 1) { // Only one item  
-                            res.send("<Response><Message>Great, I'm creating your website now.</Message></Response>");
+                            res.send("<Response><Message>You can check out your site at http://ngrok342.io/</Message></Response>");
                         } else if (Object.keys(response.outcomes[0].entities).length > 1) {
                             res.send("<Response><Message>Looks like you might have picked more than one template!</Message></Response>");
                         } else if (Object.keys(response.outcomes[0].entities).length < 1) {
@@ -53,6 +55,13 @@ exports.text = function(req, res) {
             }
         } else { // Not found, register new user
             res.send(`<Response><Message>To get started, we need access to your Facebook account. https://swyte.xyz/oauth/#!/facebook/${req.body.From.replace("+","")}</Message></Response>`);
+            var newuser = new Users({ phone: req.body.From });
+            newuser.save(function (err) {
+            	if(err) {
+            		console.log("Error " + err);
+            	}
+            	console.log("New User: " + user);
+            });
         }
     });
 };
